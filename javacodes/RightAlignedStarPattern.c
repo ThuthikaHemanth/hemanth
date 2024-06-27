72c8f10b70856ec0dffdd6a2ee4c7c372e2cc407
ThuthikaHemanth <157625359+ThuthikaHemanth@users.noreply.github.com>
2024-06-27 14:45:32 +0530

    Create RightAlignedStarPattern.c
             *
            **
           ***
          ****
         *****
        ******
       *******
      ********
     *********
    **********

---
+++ javacodes/RightAlignedStarPattern.c
@@ -0,0 +1,13 @@
+public class RightAlignedStarPattern {
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++) {
+            for (int s = 1; s <= 10 - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
